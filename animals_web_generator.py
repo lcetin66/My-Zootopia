1e9cdfd810f9abbb1da8314a54a85d3ef6e31864
lcetin66 <lcetin@3ccs.com>
2026-02-21 13:58:35 +0100

    fifth commit

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,30 +5,52 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-animals_data = load_data('animals_data.json')
+def read_and_convert_data(data):
+    """Read JSON and convert it to HTML list items"""
+    output = ""
 
-output = ''
-for info in animals_data:
-    name = info.get("name")
-    characteristics = info.get("characteristics", {})
-    diet = characteristics.get("diet")
-    locations = info.get("locations")
-    location = locations[0]
-    type_value = characteristics.get("type")
+    for animal_data in data:
+        name = animal_data.get("name", "Unknown")
 
-    output += "<li class='cards__item'>"
-    output += f"Name: {name}</br>"
-    output += f"Diet: {diet}</br>"
-    output += f"Location: {location}</br>"
-    output += f"{f'Type: {type_value}' if type_value else ''}"
-    output += f"</li>"
+        characteristics = animal_data.get("characteristics", {})
+        diet = characteristics.get("diet", "Unknown")
+        animal_type = characteristics.get("type")
 
-with open("animals_template.html", "r", encoding="utf-8") as f:
-    html = f.read()
+        locations = animal_data.get("locations", [])
+        location = locations[0] if locations else "Unknown"
 
-html = html.replace("__REPLACE_ANIMALS_INFO__", output)
+        output += "<li class='cards__item'>"
+        output += f"<div class='card__title'>{name}</div>"
+        output += "<p class='cards__text'>"
+        output += f"<strong>Diet:</strong> {diet}<br>"
+        output += f"<strong>Location:</strong> {location}<br>"
 
-with open("animals.html", "w", encoding="utf-8") as f:
-    f.write(html)
+        if animal_type:
+            output += f"<strong>Type:</strong> {animal_type}"
+
+        output += "</p>"
+        output += "</li>"
+
+    return output
+
+def write_data(output, file_path):
+    """ Writes HTML content to file """
+    with open(file_path, "r", encoding="utf-8") as file:
+        html_content = file.read()
+
+    html_content = html_content.replace('__REPLACE_ANIMALS_INFO__', output)
+
+    with open('animals.html', "w", encoding="utf-8") as file:
+        file.write(html_content)
+
+def main():
+    """ Main function """
+    data = load_data('animals_data.json')
+    output = read_and_convert_data(data)
+    write_data(output, 'animals_template.html')
+
+
+if __name__ == '__main__':
+    main()
 
 
