dc45b05080595a7d4ad5ae72a03579c527f09271
lcetin66 <lcetin@3ccs.com>
2026-02-21 11:08:04 +0100

    Third commit

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,6 +7,7 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
+output = ''
 for info in animals_data:
     name = info.get("name")
     characteristics = info.get("characteristics", {})
@@ -15,13 +16,17 @@ for info in animals_data:
     location = locations[0]
     type_value = characteristics.get("type")
 
-    print(
-        f"Name: {name}\n"
-        f"Diet: {diet}\n"
-        f"Location: {location}\n"
-        f"{f'Type: {type_value}\n' if type_value else ''}"
-    )
+    output += f"Name: {name}\n"
+    output += f"Diet: {diet}\n"
+    output += f"Location: {location}\n"
+    output += f"{f'Type: {type_value}\n' if type_value else ''}\n"
 
+with open("animals_template.html", "r", encoding="utf-8") as f:
+    html = f.read()
 
+html = html.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w", encoding="utf-8") as f:
+    f.write(html)
 
 
